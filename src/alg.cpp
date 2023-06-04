e063809387b7bf7c9f7e8f6afa7e3694d0acc4a3
gourami32 <127053477+gourami32@users.noreply.github.com>
2023-06-04 21:23:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,21 @@
-// Copyright 2022 NNTU-CS
-#include  <iostream>
-#include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "tree.h"
 
+int factorial(int n) {
+    if (n <= 1) return 1;
+    return factorial(n - 1) * n;
+}
 std::vector<char> getPerm(const Tree& tree, int n) {
   // напишите реализацию
+    if (tree.getSize() == 0 || n > factorial(tree.getSize()))
+        return {};
+    if (tree.getCh() == '*')
+        n--;
+    std::vector<char> result;
+    int temp = factorial(tree.getSize() - 1);
+    result.push_back(tree[n / temp].getCh());
+    std::vector<char> b = getPerm(tree[n / temp], n % temp);
+    result.insert(result.end(), b.begin(), b.end());
+    return result;
 }
